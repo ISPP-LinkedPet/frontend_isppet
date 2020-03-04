5cbc319f9899b7ce502630e974cea7fe648071c2
Roberto Hermoso <37594160+RobertoHermoso@users.noreply.github.com>
2020-03-04 23:46:58 +0100

    Style defined and basic compnent created
    Bootswatch css added and basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { RouterModule } from '@angular/router';
+
+//Components
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+//Services
+import {AdoptionService} from './services/adoption/adoption.service';
+import {BreedingService} from './services/breeding/breeding.service';
+import {ConfigService} from './services/config/config.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    AdoptionService,
+    BreedingService,
+    ConfigService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
