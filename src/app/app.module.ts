b86f1712a130144ec2f0b3831597e653617a01d9
evaesqmor <evaesqmor@alum.us.es>
2020-03-07 16:35:14 +0100

    Creating Breeding Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { BreedingListComponent } from './pages/breeding-list/breeding-list.component';
 import { RouterModule } from '@angular/router';
 
 //Components
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { ListComponent } from './components/list/list.component';
+import { BreedingListComponent } from './components/breeding-list/breeding-list.component';
+
+//Pages
+import { HomeComponent } from './pages/home/home.component';
+import { BreedingListPageComponent } from './pages/breeding-list-page/breeding-list-page.component';
 
 
 //Services
@@ -19,7 +21,6 @@ import {BreedingService} from './services/breeding/breeding.service';
 import {ConfigService} from './services/config/config.service';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import { BreedingListPageComponent } from './page/breeding-list-page/breeding-list-page.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,6 @@ import { BreedingListPageComponent } from './page/breeding-list-page/breeding-li
     HomeComponent,
     HeaderComponent,
     FooterComponent,
-    ListComponent,
     BreedingListComponent,
     BreedingListPageComponent,
   ],
