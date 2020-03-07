416bb1ec64a739ccfc95e267fecbf70b011f5348
David Gil Baeza <davidgil.univ@gmail.com>
2020-03-07 16:55:17 +0100

    chore(integracion): Ficheros necesarios para integración continua.
    Cambios en server.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 // server.js
 const express = require('express');
-const path = require('path');
 const app = express();
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile('index.html', {root: 'dist/angular-ispproyect/'});
 });
 
 // If an incoming request uses
@@ -31,7 +30,7 @@ app.use(forceSSL());
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/angular-ispproyect'));
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 12080);
+app.listen(process.env.PORT || 8080);
