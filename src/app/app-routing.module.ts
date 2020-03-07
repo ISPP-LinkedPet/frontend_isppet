ec422d0f0acf7477c7555967d6ba9695c875b5c4
evaesqmor <evaesqmor@alum.us.es>
2020-03-07 16:44:50 +0100

    Routing of Breeding List Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { BreedingListPageComponent } from './pages/breeding-list-page/breeding-list-page.component';
+import { from } from 'rxjs';
 
 
 const routes: Routes = [
-{path: '', component: HomeComponent}  
+{path: '', component: HomeComponent} ,
+{path: 'breedingList', component:BreedingListPageComponent} 
 ];
 
 @NgModule({
