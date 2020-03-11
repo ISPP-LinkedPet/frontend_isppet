e15b4923de73cba571678ec86f31d42ee862b8be
evaesqmor <evaesqmor@alum.us.es>
2020-03-11 17:12:09 +0100

    Project named after frontend_isppet

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angularISPProyect'`, () => {
+  it(`should have as title 'frontend_isppet'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angularISPProyect');
+    expect(app.title).toEqual('frontend_isppet');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angularISPProyect app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('frontend_isppet app is running!');
   });
 });
