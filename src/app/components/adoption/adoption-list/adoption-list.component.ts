d31a3c81786cb353d5480c82fbe64b091a3fe489
Roberto Hermoso <37594160+RobertoHermoso@users.noreply.github.com>
2020-03-14 18:00:09 +0100

    Adoption list basic

--- src/app/components/adoption/adoption-list/adoption-list.component.ts
+++ src/app/components/adoption/adoption-list/adoption-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import {AdoptionService} from '../../../services/adoption/adoption.service';
+
 @Component({
   selector: 'app-adoption-list',
   templateUrl: './adoption-list.component.html',
   styleUrls: ['./adoption-list.component.css']
 })
 export class AdoptionListComponent implements OnInit {
-
-  constructor() { }
+  adoptions = new Array();
+  constructor(private adoptionService: AdoptionService) { }
 
   ngOnInit(): void {
+    this.adoptionService.getAllAdoptions(localStorage.getItem('access_token')).then(res=>res.forEach(element => {
+      this.adoptions.push(element)
+    }))
+    console.log(this.adoptions)
   }
 
 }
--- src/app/services/adoption/adoption.service.ts
+++ src/app/services/adoption/adoption.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import {environment} from '../../../environments/environment';
+import {RequestService} from '../request/request.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdoptionService {
 
-  constructor() { }
+  constructor(private requestService: RequestService) { }
+
+  getAllAdoptions(token: string) {
+    return this.requestService.request('GET', `${environment.endpoint}/adoption/particular`, {}, {access_token: token}, true);
+  }
 }
