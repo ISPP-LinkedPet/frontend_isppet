23a00c93dd1d96256a067c098dc5f32e41f9c5a7
Yerehia <andangrei@alum.us.es>
2020-03-16 19:59:43 +0100

    fix(): rol buttons fixed to restrict access to actions

--- src/app/components/adoption/adoption-list/adoption-list.component.ts
+++ src/app/components/adoption/adoption-list/adoption-list.component.ts
@@ -4,6 +4,7 @@ import {AdoptionService} from '../../../services/adoption/adoption.service';
 import {AdoptionListPageComponent} from '../../../pages/adoption/list/adoption-list-page.component'
 import {AdoptionDisplayComponent} from '../adoption-display/adoption-display.component';
 import {environment} from '../../../../environments/environment';
+import { ConfigService } from 'src/app/services/config/config.service';
 
 @Component({
   selector: 'app-adoption-list',
@@ -14,7 +15,7 @@ export class AdoptionListComponent implements OnInit {
   adoptions = new Array();
   env = environment.endpoint;
   public adoptionDisplayComponent: AdoptionDisplayComponent;
-  constructor(private adoptionService: AdoptionService, public adoptionListPageComponent: AdoptionListPageComponent) { }
+  constructor(private configService: ConfigService,private adoptionService: AdoptionService, public adoptionListPageComponent: AdoptionListPageComponent) { }
 
   ngOnInit(): void {
     this.adoptionService.getAllAdoptions(localStorage.getItem('access_token')).then(res=>res.forEach(element => {
@@ -22,6 +23,9 @@ export class AdoptionListComponent implements OnInit {
     }))
     console.log(this.adoptions)
   }
+
+  userlogged = this.configService.getUserLogged();
+  rol: string = this.userlogged ? this.userlogged.role : 'disconnected';
 
   viewDetails(id: string){
     this.adoptionListPageComponent.isLeftVisible = !this.adoptionListPageComponent.isLeftVisible;
--- src/app/components/breeding/breeding-list/breeding-list.component.ts
+++ src/app/components/breeding/breeding-list/breeding-list.component.ts
@@ -3,6 +3,7 @@ import { BreedingService } from '../../../services/breeding/breeding.service';
 import {BreedingListPageComponent} from '../../../pages/breeding/list/breeding-list-page.component'
 import {BreedingDisplayComponent} from 'src/app/components/breeding/breeding-display/breeding-display.component';
 import {environment} from 'src/environments/environment';
+import { ConfigService } from 'src/app/services/config/config.service';
 
 @Component({
   selector: 'app-breeding-list',
@@ -16,7 +17,8 @@ export class BreedingListComponent implements OnInit {
   public breedingDisplayComponent: BreedingDisplayComponent;
   constructor(
       private breedingService: BreedingService,
-      public breedingListPageComponent: BreedingListPageComponent
+      public breedingListPageComponent: BreedingListPageComponent,
+      private configService: ConfigService
   ) {}
 
   ngOnInit(): void {
@@ -26,6 +28,9 @@ export class BreedingListComponent implements OnInit {
       }));
       console.log(this.breedings);
   }
+
+  userlogged = this.configService.getUserLogged();
+  rol: string = this.userlogged ? this.userlogged.role : 'disconnected';
 
   viewDetails(id: string){
     this.breedingListPageComponent.isLeftVisible = !this.breedingListPageComponent.isLeftVisible;
