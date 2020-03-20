2422eb745a3065aaf8af1763fcf4f362a6e6c1ca
evaesqmor <evaesqmor@alum.us.es>
2020-03-20 23:00:22 +0100

    fix(breeding & adoption): Personal Breedings & Personal Adoptions Lists

--- src/app/pages/breeding/edit-particular/edit-particular.component.ts
+++ src/app/pages/breeding/edit-particular/edit-particular.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BreedingService } from 'src/app/services/breeding/breeding.service';
+import { ActivatedRoute } from '@angular/router';
+import { ConfigService } from 'src/app/services/config/config.service';
 
 @Component({
   selector: 'app-edit-particular',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditParticularComponent implements OnInit {
 
-  constructor() { }
+  constructor(private breedingService: BreedingService, private route: ActivatedRoute, public configService: ConfigService) { }
+
+  id = null;
+  breeding = null;
+  private sub: any;
+
+  userlogged = this.configService.getUserLogged();
+  rol: string = this.userlogged ? this.userlogged.role : 'disconnected';
 
   ngOnInit(): void {
+    this.sub = this.route.params.subscribe(params => {
+      this.id = +params[' id ']; // (+) converts string 'id' to a number
+    });
+    this.breedingService.getBreedingById(this.id).then(res => this.breeding = res.breeding);
   }
 
 }
