00af3e68192c1604b0dcd0248b6fb72f35e9a61d
Christian-martinez-andrades <chrmarand@alum.us.es>
2020-03-23 20:45:32 +0100

    feature(user): Register form
    Added errors.

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -9,56 +9,64 @@ import { LoginService } from '../../../services/login/login.service';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-  userName: string;
-  role: string;
-  password: string;
-  repeatPassword: string;
-  name: string;
-  email: string;
-  address: string;
-  telephone: string;
-  optionalPhoto: string;
-  surname: string; // only particular
-
   showParticularInputs: boolean;
-
   isValid: boolean;
+  isValidUserName: boolean;
+  isValidRole: boolean;
+  isValidPassword: boolean;
+  isValidRepeatPassword: boolean;
+  isValidName: boolean;
+  isValidEmail: boolean;
+  isValidAddress: boolean;
+  isValidTelephone: boolean;
+  isValidOptionalPhoto: boolean;
+  isValidSurname: boolean;
 
   registerForm: any;
-
-  constructor(public homeComponent: LoginRegisterComponent, public loginService: LoginService) {}
+  role: string;
+  constructor(public homeComponent: LoginRegisterComponent, public loginService: LoginService) { }
 
   ngOnInit(): void {
     this.registerForm = new FormGroup({
-      user_name: new FormControl(this.userName, [Validators.required]),
-      role: new FormControl(this.role, [Validators.required]),
-      password: new FormControl(this.password, [Validators.required]),
-      repeat_password: new FormControl(this.repeatPassword, [
+      user_name: new FormControl('', [Validators.required, Validators.minLength(4)]),
+      role: new FormControl(this.role || ''),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+      repeat_password: new FormControl('', [
         Validators.required,
       ]),
-      name: new FormControl(this.name, [Validators.required]),
-      email: new FormControl(this.email, [Validators.required]),
-      address: new FormControl(this.address, [Validators.required]),
-      telephone: new FormControl(this.telephone, [Validators.required]),
-      optional_photo: new FormControl(this.optionalPhoto, [
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      address: new FormControl('', [Validators.required]),
+      telephone: new FormControl('', [Validators.required, Validators.minLength(9), Validators.maxLength(9)]),
+      optional_photo: new FormControl('', [
         Validators.required,
       ]),
-      surname: new FormControl(this.optionalPhoto),
+      surname: new FormControl('', [Validators.required])
     });
 
     this.isValid = false;
+    this.isValidUserName = true;
+    this.isValidRole = true;
+    this.isValidPassword = true;
+    this.isValidRepeatPassword = true;
+    this.isValidName = true;
+    this.isValidEmail = true;
+    this.isValidAddress = true;
+    this.isValidTelephone = true;
+    this.isValidOptionalPhoto = true;
+    this.isValidSurname = true;
     this.showParticularInputs = this.registerForm.value.role === 'particular';
   }
 
   onSubmit() {
-    if (this.registerForm.value.role === 'particular') {
-      this.checkParticular();
-    } else if (this.registerForm.value.role === 'shelter') {
-      this.checkShelter();
-    } else {
-      this.isValid = false;
-    }
-
+    // if (this.registerForm.value.role === 'particular') {
+    //   this.checkParticular();
+    // } else if (this.registerForm.value.role === 'shelter') {
+    //   this.checkShelter();
+    // } else {
+    //   this.isValid = false;
+    // }
+    this.validationFields();
     if (this.isValid) {
       this.loginService
         .register(this.registerForm.value)
@@ -75,6 +83,87 @@ export class RegisterComponent implements OnInit {
   }
 
   onChangeRole(e: Event) {
-      this.showParticularInputs = e.toString() === 'particular';
+    this.showParticularInputs = e.toString() === 'particular';
+    this.validateRole();
+  }
+
+  validationFields() {
+    this.validateAddress();
+    this.validateEmail();
+    this.validateName();
+    this.validatePassword();
+    this.validateRepeatPassword();
+    this.validateSurname();
+    this.validateTelephone();
+    this.validateUsername();
+    this.validateRole();
+  }
+
+  validateUsername() {
+    this.isValidUserName = this.registerForm.get('user_name').valid;
+    if (!this.isValidUserName) {
+      this.isValid = false;
+    }
+  }
+
+  validatePassword() {
+    this.isValidPassword = this.registerForm.get('password').valid;
+    if (!this.isValidPassword) {
+      this.isValid = false;
+    }
+  }
+
+  validateRepeatPassword() {
+    this.isValidRepeatPassword = this.registerForm.get('repeat_password').valid &&
+      this.registerForm.value.password === this.registerForm.value.repeat_password;
+    if (!this.isValidRepeatPassword) {
+      this.isValid = false;
+    }
+  }
+
+  validateName() {
+    this.isValidName = this.registerForm.get('name').valid;
+    if (!this.isValidName) {
+      this.isValid = false;
+    }
+  }
+
+  validateEmail() {
+    this.isValidEmail = this.registerForm.get('email').valid;
+    if (!this.isValidEmail) {
+      this.isValid = false;
+    }
+  }
+
+  validateAddress() {
+    this.isValidAddress = this.registerForm.get('address').valid;
+    if (!this.isValidAddress) {
+      this.isValid = false;
+    }
+  }
+
+  validateTelephone() {
+    this.isValidTelephone = this.registerForm.get('telephone').valid;
+    if (!this.isValidTelephone) {
+      this.isValid = false;
+    }
+  }
+
+  validateSurname() {
+    if (this.registerForm.value.role === 'particular') {
+      this.isValidSurname = this.registerForm.get('surname').valid;
+      if (!this.isValidSurname) {
+        this.isValid = false;
+      }
+    } else {
+      this.isValidSurname = true;
+    }
+  }
+  validateRole() {
+    this.isValidRole = ['particular', 'shelter'].includes(this.registerForm.get('role').value);
+    console.log(this.isValidRole);
+    if (!this.isValidRole) {
+      this.isValid = false;
+    }
   }
 }
