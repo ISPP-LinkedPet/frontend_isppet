bca23a97a809983f72c6bcdb515c9be997f0f936
Daniel Negrón <danielnf@hotmail.es>
2020-03-29 18:25:29 +0200

    Tests Travis

--- karma.conf.js
+++ karma.conf.js
@@ -33,6 +33,10 @@ module.exports = function (config) {
             }
         },
         singleRun: false,
-        restartOnFileChange: true
+        restartOnFileChange: true,
+        captureTimeout: 210000,
+        browserDisconnectTolerance: 3,
+        browserDisconnectTimeout : 210000,
+        browserNoActivityTimeout : 210000
     });
 };
