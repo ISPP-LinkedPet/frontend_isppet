92be17304684882977aaa7a7df470dc44450d011
Mcarmen <maricarmenarenas98@hotmail.es>
2020-03-31 21:14:36 +0200

    fix(): CI / CD

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  endpoint: 'https://backend-linkedpet-prod-v1.herokuapp.com',
+  stripe_key: 'pk_test_1VBNlqdJTw6mXmdOxxsLbIn400SyJriI9f',
   mapbox_key: 'pk.eyJ1IjoiYWJkbW9oIiwiYSI6ImNrODYzeXJjYjA5enMzbm84Zzg1dDk5cHcifQ.bvP729bcquda2cj-y4fQdQ',
-  endpoint: 'https://backend-linkedpet-dev.herokuapp.com'
 };
