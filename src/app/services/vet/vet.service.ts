cc3d1f02cdbf46d7fb5c074cf031f9b390c3a9a7
Christian-martinez-andrades <chrmarand@alum.us.es>
2020-04-04 19:05:02 +0200

    feat(vet): Vet premium Hacer deshacer premium por parte de administrador y mostrar imágenes de premium y recomendado.

--- src/app/services/vet/vet.service.ts
+++ src/app/services/vet/vet.service.ts
@@ -1,5 +1,5 @@
-import {environment} from '../../../environments/environment';
-import {RequestService} from '../request/request.service';
+import { environment } from '../../../environments/environment';
+import { RequestService } from '../request/request.service';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -15,5 +15,11 @@ export class VetService {
 
   getVetAdvertisements() {
     return this.requestService.request('GET', `${environment.endpoint}/ad/2`, {}, {}, true);
+  }
+  changePremium(id: number) {
+    return this.requestService.request('PUT', `${environment.endpoint}/vet/premiumTrue/${id}`, {}, {}, true);
+  }
+  changeNormal(id: number) {
+    return this.requestService.request('PUT', `${environment.endpoint}/vet/premiumFalse/${id}`, {}, {}, true);
   }
 }
