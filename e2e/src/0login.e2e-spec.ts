2e341bd6cfd1dfe8945e7d827b54c890a2afbff2
David Gil Baeza <davidgil.univ@gmail.com>
2020-04-13 23:06:11 +0200

    Arreglados tests protractor

--- e2e/src/0login.e2e-spec.ts
+++ e2e/src/0login.e2e-spec.ts
@@ -3,18 +3,17 @@ import {browser, by, element, logging} from 'protractor';
 
 // browser.executeScript('window.scrollTo(0, document.body.scrollHeight)');
 
-describe('Listado crianza', () => {
+describe('Login', () => {
   let page: IndexPage;
 
   beforeEach(() => {
     page = new IndexPage();
   });
 
-  it('Debe iniciar sesion, listar las crianzas, filtrar y acceder a un detalle', () => {
+  it('Debe iniciar sesion', () => {
     page.navigateTo();
 
     browser.driver.manage().window().setSize(1536, 824);
-    browser.sleep(20000); // Espera para que arranque heroku en el caso de estar dormido el servidor
     element(by.css('ul:nth-of-type(4)>li:nth-of-type(1)>a')).click();
     element(by.css('fieldset>div:nth-of-type(1)>input')).click();
     // @ts-ignore
@@ -22,21 +21,9 @@ describe('Listado crianza', () => {
     // @ts-ignore
     element(by.css('div:nth-of-type(2)>input')).clear().sendKeys('hola');
     element(by.css('fieldset>button:nth-of-type(1)')).click();
-    element(by.css('ul:nth-of-type(1)>li:nth-of-type(2)>a')).click();
-    element(by.css('form>div:nth-of-type(1)>div:nth-of-type(1)>input')).click();
-    // @ts-ignore
-    element(by.css('form>div:nth-of-type(1)>div:nth-of-type(1)>input')).clear().sendKeys('23.5');
-    element(by.css('slide-panel>div:nth-of-type(1)>div:nth-of-type(1)>div:nth-of-type(1)')).click();
 
-    // IR AL DETALLE
-    browser.sleep(500);
-    browser.executeScript('window.scrollTo(0, document.body.scrollHeight)');
-    browser.sleep(500);
-
-    element(by.css('div:nth-of-type(3)>div>button')).click();
-    browser.sleep(1000);
-    // FIN
-
+    page.navigateTo();
+    browser.sleep(20000); // Espera para que arranque heroku en el caso de estar dormido el servidor
   });
 
   afterEach(async () => {
