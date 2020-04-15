5f6debc107d902b14b9b34eaadf84961f958a3b0
Mcarmen <maricarmenarenas98@hotmail.es>
2020-04-15 18:27:01 +0200

    fix(endpoint): deploy dev

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  endpoint: 'http://localhost:3000',
+  endpoint: 'https://backend-linkedpet-dev.herokuapp.com',
   stripe_key: 'pk_test_1VBNlqdJTw6mXmdOxxsLbIn400SyJriI9f',
   mapbox_key: 'pk.eyJ1IjoiYWJkbW9oIiwiYSI6ImNrODYzeXJjYjA5enMzbm84Zzg1dDk5cHcifQ.bvP729bcquda2cj-y4fQdQ',
 };
