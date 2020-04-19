8ea0859bd81f4b10d98218cf006c48255dd7a4de
jesushd103 <jhd103@gmail.com>
2020-04-19 17:33:47 +0200

    fix(orthography):

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  endpoint: 'https://backend-linkedpet-dev.herokuapp.com',
+  endpoint: 'localhost:3000',
   stripe_key: 'pk_test_1VBNlqdJTw6mXmdOxxsLbIn400SyJriI9f',
   mapbox_key: 'pk.eyJ1IjoiYWJkbW9oIiwiYSI6ImNrODYzeXJjYjA5enMzbm84Zzg1dDk5cHcifQ.bvP729bcquda2cj-y4fQdQ',
   sleep_tests: false
